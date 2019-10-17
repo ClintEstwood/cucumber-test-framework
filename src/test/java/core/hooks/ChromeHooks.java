d0d88c7a588bad28696d454a6fe97d77a57d689d
Sergey Trusilnikov <STrusilnikov@gmail.com>
2019-10-18 01:32:19 +0200

    console messages like 'Starting ChromeDriver 2.43.600233 (523efee95e3d68b8719b3a1c83051aa63aa6b10d) on port 1628 Only local connections are allowed.' deleted

--- src/test/java/core/hooks/ChromeHooks.java
+++ src/test/java/core/hooks/ChromeHooks.java
@@ -4,6 +4,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeHooks {
     public static WebDriver driver;
@@ -11,7 +12,9 @@ public class ChromeHooks {
     @Before
     public void beforeScenario(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("webdriver.chrome.silentOutput", "true");
+        driver = new ChromeDriver(chromeOptions);
     }
 
     @After
