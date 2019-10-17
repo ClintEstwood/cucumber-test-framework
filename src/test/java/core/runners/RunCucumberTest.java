1dbee93f1c32fe37e686fa1c5578a164392c22a2
Sergey Trusilnikov <STrusilnikov@gmail.com>
2019-10-18 01:04:06 +0200

    RunCucumberTest: * path to features added via features option; * paths to step definitions and hooks added via glue

--- src/test/java/core/runners/RunCucumberTest.java
+++ src/test/java/core/runners/RunCucumberTest.java
@@ -3,6 +3,17 @@ package core.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+        plugin = {
+                "pretty"
+        },
+        features = {
+                "src/test/resources/features"
+        },
+        glue = {
+                "stepDefinitions",
+                "core.hooks"
+        }
+)
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
