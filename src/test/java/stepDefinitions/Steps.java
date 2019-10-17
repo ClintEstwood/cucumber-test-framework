988395539cd1827521017b2952e86a2838dfdf51
Sergey Trusilnikov <STrusilnikov@gmail.com>
2019-10-18 00:50:02 +0200

    Step 'I run Chrome browser' developed

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,11 +1,14 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Steps {
     @Given("I run Chrome browser")
     public void i_run_chrome_browser(){
-        //todo: add code after webdriver install
+        WebDriver driver = new ChromeDriver();
+        driver.get("www.Google.com");
     }
 
 }
