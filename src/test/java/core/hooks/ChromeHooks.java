01b0c41f242d4d41e6b9aa0b2c5e557a7b4e6961
Sergey Trusilnikov <STrusilnikov@gmail.com>
2019-10-18 01:53:40 +0200

    ChromeHooks: hide webdriwer messages like AM org.openqa.selenium.remote.ProtocolHandshake createSession INFO: Detected dialect: W3C

--- src/test/java/core/hooks/ChromeHooks.java
+++ src/test/java/core/hooks/ChromeHooks.java
@@ -5,6 +5,7 @@ import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.logging.Level;
 
 public class ChromeHooks {
     public static WebDriver driver;
@@ -13,6 +14,7 @@ public class ChromeHooks {
     public void beforeScenario(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.silentOutput", "true");
+        java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("start-maximized");
         driver = new ChromeDriver(chromeOptions);
