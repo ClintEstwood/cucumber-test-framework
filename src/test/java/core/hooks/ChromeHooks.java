75f565452e64559d2de3a0ac8e34913c1ce1c86d
Sergey Trusilnikov <STrusilnikov@gmail.com>
2019-10-18 01:27:39 +0200

    webdriver initialization moved from step definition to the hook class; After test update: browser close added

--- src/test/java/core/hooks/ChromeHooks.java
+++ src/test/java/core/hooks/ChromeHooks.java
@@ -2,16 +2,20 @@ package core.hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeHooks {
+    public static WebDriver driver;
 
     @Before
     public void beforeScenario(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
+        driver = new ChromeDriver();
     }
 
     @After
     public void afterScenario(){
-        System.out.println("This will run after the Scenario");
+        driver.close();
     }
 }
--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,14 +1,12 @@
 package stepDefinitions;
 
+import core.hooks.ChromeHooks;
 import io.cucumber.java.en.Given;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Steps {
     @Given("I run Chrome browser")
     public void i_run_chrome_browser(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.Google.com");
+        ChromeHooks.driver.get("https://www.Google.com");
     }
 
 }
