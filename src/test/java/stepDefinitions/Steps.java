04b7663f0d9c99ad118af233e481b964310c54f1
Sergey Trusilnikov <STrusilnikov@gmail.com>
2019-10-18 00:33:16 +0200

    Steps initial commit

---
+++ src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,11 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class Steps {
+    @Given("I run Chrome browser")
+    public void i_run_chrome_browser(){
+        //todo: add code after webdriver install
+    }
+
+}
