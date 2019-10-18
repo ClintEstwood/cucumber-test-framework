09ca32a7609b847cb472533db6ffc856058c0508
Sergey Trusilnikov <STrusilnikov@gmail.com>
2019-10-18 20:02:06 +0200

    step definition I go to the (.*?) page changed to I go to the {string} page - it looks better

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -4,7 +4,7 @@ import core.hooks.ChromeHooks;
 import io.cucumber.java.en.Given;
 
 public class Steps {
-    @Given("I go to the (.*?) page")
+    @Given("I go to the {string} page")
     public void i_run_chrome_browser(String url){
         ChromeHooks.driver.get("https://" + url);
     }
