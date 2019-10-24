fbe2306233c3b3194178ca99f04a7d46c6e0cb5c
Sergey Trusilnikov <STrusilnikov@gmail.com>
2019-10-24 08:33:37 +0200

    step method renamed

--- src/main/java/stepDefinitions/Steps.java
+++ src/main/java/stepDefinitions/Steps.java
@@ -1,11 +1,11 @@
-package core.stepDefinitions;
+package stepDefinitions;
 
 import core.hooks.ChromeHooks;
 import io.cucumber.java.en.Given;
 
 public class Steps {
     @Given("I go to the {string} page")
-    public void i_run_chrome_browser(String url){
+    public void i_go_to_the_page(String url){
         ChromeHooks.driver.get("https://" + url);
     }
 }
