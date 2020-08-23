170ff287ed8cbe29ddaa27f8f9fc4b8a00bcf526
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-23 17:46:20 +0200

    EcbEuropaExchangePage refactor: + PageActions implementation added; access to elements by labels added for steps like 'I click on 'elementName' on European Central Bank Main page

--- src/main/java/pages/EcbEuropaExchangePage.java
+++ src/main/java/pages/EcbEuropaExchangePage.java
@@ -1,20 +1,33 @@
 package pages;
 
+import core.pages.PageActions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class EcbEuropaExchangePage {
+public class EcbEuropaExchangePage implements PageActions {
+
+    @Override
+    public WebElement getElementByName(String elementName) {
+        return switch (elementName.toUpperCase()) {
+            case "EXCHANGE TABLE" -> exchangeTable;
+            case "EXCHANGE TABLE ROW" -> exchangeTableRow;
+            case "EXCHANGE TABLE CURRENCY CELL" -> exchangeTableCurrencyCell;
+            case "EXCHANGE TABLE RATE CELL" -> exchangeTableRateCell;
+            default -> throw new IllegalStateException("Can't find element with name: " + elementName +
+                    " on page 'Euro foreign exchange reference rates'");
+        };
+    }
 
     @FindBy(css = "div[class='forextable']")
-    public WebElement exchangeTable;
+    private static WebElement exchangeTable;
 
     @FindBy(css = "tbody tr")
-    public WebElement exchangeTableRow;
+    private static WebElement exchangeTableRow;
 
     @FindBy(css = "td[class='currency']")
-    public WebElement exchangeTableCurrencyCell;
+    private static WebElement exchangeTableCurrencyCell;
 
     @FindBy(css = "td[class='spot number'] span[class='rate']")
-    public WebElement exchangeTableRateCell;
+    private static WebElement exchangeTableRateCell;
 
 }
