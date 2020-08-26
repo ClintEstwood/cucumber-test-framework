682161be4f74815ed861bba7dd23958b31be0b89
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-26 19:34:41 +0200

    CommonSteps class added; Step 'Verify that variables {string} and {string} are the same' added;

---
+++ src/main/java/stepDefinitions/CommonSteps.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import core.ScenarioContext;
+import io.cucumber.java.en.Given;
+import org.assertj.core.api.Assertions;
+
+public class CommonSteps {
+
+    private ScenarioContext scenarioContext;
+
+    public CommonSteps(ScenarioContext scenarioContext){
+        this.scenarioContext = scenarioContext;
+    }
+
+    @Given("Verify that variables {string} and {string} are the same")
+    public void iGetLatestForeignExchangeRatesViaRESTAPI(String variable1, String variable2) {
+        Assertions.assertThat(scenarioContext.getContext(variable1)).isEqualTo(scenarioContext.getContext(variable2));
+    }
+}
