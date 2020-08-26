793da1675ade1fcac6ba3c43ec79789340969de6
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-26 13:02:52 +0200

    new chromedriver ver 84 added;

--- src/main/java/core/hooks/ChromeHooks.java
+++ src/main/java/core/hooks/ChromeHooks.java
@@ -1,18 +1,17 @@
 package core.hooks;
 
+import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.logging.Level;
 
 public class ChromeHooks {
-    public static WebDriver driver;
 
     @Before
     public void beforeScenario(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver_84.exe");
         System.setProperty("webdriver.chrome.silentOutput", "true");
         java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
         ChromeOptions chromeOptions = new ChromeOptions();
