30756c980a11112379971d6f137eb2e95e9aeae7
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-26 19:35:21 +0200

    Exchange: toString now with sort;
    override methods added:
    equals
    hashCode

--- src/main/java/core/models/Exchange.java
+++ src/main/java/core/models/Exchange.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -18,7 +20,23 @@ public class Exchange {
 
     @Override
     public String toString() {
+        Collections.sort(rates);
         return String.format("Exchange { base = '%s', rates = {" + rates + "}, date = '%s' }", base, date);
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Exchange exchange = (Exchange) o;
+        return Objects.equals(base, exchange.base) &&
+                Objects.equals(rates, exchange.rates) &&
+                Objects.equals(date, exchange.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(base, rates, date);
+    }
 }
