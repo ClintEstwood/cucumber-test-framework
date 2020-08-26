edb665e76d332b309d17b492a492b5b612c3a85e
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-26 13:04:51 +0200

    EcbEuropaExchangePage refactor: returns By's instead of WebElement

--- src/main/java/pages/EcbEuropaExchangePage.java
+++ src/main/java/pages/EcbEuropaExchangePage.java
@@ -1,33 +1,29 @@
 package pages;
 
 import core.pages.PageActions;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.By;
 
 public class EcbEuropaExchangePage implements PageActions {
 
+    private static final By exchangeTable = By.cssSelector("div[class='forextable']");
+    private static final By exchangeTableRow = By.cssSelector("tbody tr");
+    private static final By exchangeTableCurrencyCell = By.cssSelector("td[class='currency']");
+    private static final By exchangeTableRateCell = By.cssSelector("td[class='spot number'] span[class='rate']");
+    private static final By exchangeDate = By.xpath("//h2[contains(text(),'Euro foreign exchange reference rates: ')]");
+
     @Override
-    public WebElement getElementByName(String elementName) {
+    public By getSelectorByName(String elementName) {
         return switch (elementName.toUpperCase()) {
             case "EXCHANGE TABLE" -> exchangeTable;
             case "EXCHANGE TABLE ROW" -> exchangeTableRow;
             case "EXCHANGE TABLE CURRENCY CELL" -> exchangeTableCurrencyCell;
             case "EXCHANGE TABLE RATE CELL" -> exchangeTableRateCell;
+            case "EXCHANGE DATE" -> exchangeDate;
             default -> throw new IllegalStateException("Can't find element with name: " + elementName +
                     " on page 'Euro foreign exchange reference rates'");
         };
     }
 
-    @FindBy(css = "div[class='forextable']")
-    private static WebElement exchangeTable;
 
-    @FindBy(css = "tbody tr")
-    private static WebElement exchangeTableRow;
-
-    @FindBy(css = "td[class='currency']")
-    private static WebElement exchangeTableCurrencyCell;
-
-    @FindBy(css = "td[class='spot number'] span[class='rate']")
-    private static WebElement exchangeTableRateCell;
 
 }
