71a0bc5d4d71226270e96746d8d1ba6736701429
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-28 14:38:05 +0200

    step 'Verify that exchange rates in variable {string} has listed records:' fixed

--- src/main/java/stepDefinitions/CommonSteps.java
+++ src/main/java/stepDefinitions/CommonSteps.java
@@ -56,6 +56,6 @@ public class CommonSteps {
         dataTable.asMap(String.class, Float.class).forEach(((symbol, rate) ->
                         expectedRates.add(new Rate((String) symbol, (Float) rate))));
         Collections.sort(expectedRates);
-        Assertions.assertThat(actualExchange.getRates()).containsExactlyElementsOf(expectedRates);
+        Assertions.assertThat(actualExchange.getRates()).containsExactlyInAnyOrderElementsOf(expectedRates);
     }
 }
