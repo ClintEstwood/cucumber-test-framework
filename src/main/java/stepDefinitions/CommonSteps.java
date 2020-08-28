65a1ec49526aaca5800a38ef9534c21ca6f657ef
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-28 11:49:40 +0200

    new step added: 'Verify that exchange rates in variable {string} has base {string}'

--- src/main/java/stepDefinitions/CommonSteps.java
+++ src/main/java/stepDefinitions/CommonSteps.java
@@ -1,7 +1,9 @@
 package stepDefinitions;
 
 import core.ScenarioContext;
+import core.models.Exchange;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import org.assertj.core.api.Assertions;
 
 public class CommonSteps {
@@ -15,5 +17,11 @@ public class CommonSteps {
     @Given("Verify that variables {string} and {string} are the same")
     public void iGetLatestForeignExchangeRatesViaRESTAPI(String variable1, String variable2) {
         Assertions.assertThat(scenarioContext.getContext(variable1)).isEqualToComparingOnlyGivenFields(scenarioContext.getContext(variable2));
+    }
+
+    @Then("Verify that exchange rates in variable {string} has base {string}")
+    public void verifyThatExchangeRatesInVariableHasBase(String variableName, String expectedBase) {
+        Exchange exchange = (Exchange) scenarioContext.getContext(variableName);
+        Assertions.assertThat(exchange.getBase()).isEqualTo(expectedBase);
     }
 }
