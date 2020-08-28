de47e5566815bf76b373d2ff9c3e989b72a8d2c6
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-28 11:32:58 +0200

    new step added: 'I get latest foreign rates for exchange(s) {string} via REST API and save it into variable {string}'

--- src/main/java/stepDefinitions/RestSteps.java
+++ src/main/java/stepDefinitions/RestSteps.java
@@ -8,12 +8,19 @@ public class RestSteps {
 
     private ScenarioContext scenarioContext;
 
-    public RestSteps(ScenarioContext scenarioContext){
+    public RestSteps(ScenarioContext scenarioContext) {
         this.scenarioContext = scenarioContext;
     }
 
     @Given("I get latest foreign exchange rates via REST API and save it into variable {string}")
     public void iGetLatestForeignExchangeRatesViaRESTAPI(String variableName) {
         scenarioContext.setContext(variableName, GetRestData.getLatestForeignExchangeRates());
+    }
+
+    //exchanges example: "USD,GBP,PLN"
+    @Given("I get latest foreign rates for exchange(s) {string} via REST API and save it into variable {string}")
+    public void iGetLatestForeignRatesForExchangeSStringViaRESTAPIAndSaveItIntoVariableString(String exchanges, String variableName) {
+        scenarioContext.setContext(variableName, GetRestData.getLatestForeignExchangeRatesWithSymbols(exchanges));
+
     }
 }
