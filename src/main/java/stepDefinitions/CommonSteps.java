ffd60a8c5e3bf4adf5183fb019b16480adcee006
Sergey Trusilnikov <STrusilnikov@gmail.com>
2020-08-28 13:48:16 +0200

    steps added: 'Verify that exchange rates in variable {string} has date {string}' 'Verify that exchange rates in variable {string} has listed records:'

--- src/main/java/stepDefinitions/CommonSteps.java
+++ src/main/java/stepDefinitions/CommonSteps.java
@@ -2,13 +2,21 @@ package stepDefinitions;
 
 import core.ScenarioContext;
 import core.models.Exchange;
+import core.models.Rate;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.datatable.internal.difflib.Delta;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.apache.commons.lang3.StringUtils;
 import org.assertj.core.api.Assertions;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class CommonSteps {
 
@@ -35,5 +43,21 @@ public class CommonSteps {
         List<String> actualSymbols = new ArrayList<>();
         exchange.getRates().forEach(rate -> actualSymbols.add(rate.getExchange()));
         Assertions.assertThat(actualSymbols).contains(StringUtils.split(expectedSymbols, ","));
+    }
+
+    @Then("Verify that exchange rates in variable {string} has date {string}")
+    public void verifyThatExchangeRatesInVariableHasDate(String variableName, String expectedDate) {
+        Exchange exchange = (Exchange) scenarioContext.getContext(variableName);
+        LocalDate expectedLocalDate = LocalDate.parse(expectedDate, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        Assertions.assertThat(exchange.getDate()).isEqualTo(expectedLocalDate);
+    }
+
+    @Then("Verify that exchange rates in variable {string} has listed records:")
+    public void verifyThatExchangeRatesInVariableHasListedRecords(String variableName, DataTable dataTable) {
+        Exchange exchange = (Exchange) scenarioContext.getContext(variableName);
+        dataTable.asMap(String.class, Float.class).forEach(((symbol, rate) ->
+                Assertions.assertThat(exchange.getRates())
+                        .contains(new Rate((String) symbol, (Float) rate))
+        ));
     }
 }
