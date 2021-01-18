df0b69808b943fb0ba08daeca56fd673e41386cc
Sergey <strusilnikov@gmail.com>
2021-01-18 13:57:20 +0100

    chromedriver ver 87 added; CHROME_BINARY_PATH added

--- src/main/java/core/hooks/ChromeHooks.java
+++ src/main/java/core/hooks/ChromeHooks.java
@@ -9,12 +9,16 @@ import java.util.logging.Level;
 
 public class ChromeHooks {
 
+    private String CHROME_BINARY_PATH = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+    private String CHROME_DRIVER_PATH = "src/test/resources/webdrivers/chromedriver_87.exe";
+
     @Before
     public void beforeScenario(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver_84.exe");
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
         System.setProperty("webdriver.chrome.silentOutput", "true");
         java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
         ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary(CHROME_BINARY_PATH);
         chromeOptions.addArguments("start-maximized");
 
         WebDriverRunner.setWebDriver(new ChromeDriver(chromeOptions));
